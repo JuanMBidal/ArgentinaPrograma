29efacfffa8862425dc53c9808b3b22867ca86dc
Juan Bidal <juanmbidal@gmail.com>
2023-05-20 11:34:51 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,6 @@
+let base = 12.5;
+let altura = 5.3;
+let area = base*altura;
+console.log(area);
+// a) Si la base es de 5cm y la altura 3cm el área es de 15cm.
+// b) Si la base es 12.5cm y la altura 5.3cm el área es 66.25cm.
---
+++ practico0/promedio3.js
@@ -0,0 +1,8 @@
+let num1 = -1.2;
+let num2 = 5.3;
+let num3 = -3.3;
+let promedio = (num1 + num2 + num3) / 3;
+console.log(promedio);
+//a. 7
+//b. -4.3
+//c. 0.26
